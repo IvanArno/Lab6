9d0c1e6959eb31d2bda7b93f04ecd26f52b7c3c0
IvanArno <ivanarnautovskii@gmail.com>
2024-11-10 12:47:38 +0300

    lab6

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"edu.java.lab2","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"edu.java.lab2","c":"App","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"edu.java.lab2","c":"App","l":"show()"}];updateSearchResults();
